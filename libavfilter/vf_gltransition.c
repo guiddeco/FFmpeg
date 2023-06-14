046ed3a63dfcdd4b2718a4b08cfb94d167b657fc
Michael Smirnov <michael@guidde.co>
2023-06-14 09:19:09 -0600

    make libavfilter/vf_gltransition.c compilation to work

--- libavfilter/vf_gltransition.c
+++ libavfilter/vf_gltransition.c
@@ -9,22 +9,22 @@
 #include "framesync.h"
 
 #ifndef __APPLE__
-# define GL_TRANSITION_USING_EGL //remove this line if you don't want to use EGL
+#define GL_TRANSITION_USING_EGL // remove this line if you don't want to use EGL
 #endif
 
 #ifdef __APPLE__
-# define __gl_h_
-# define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
-# include <OpenGL/gl3.h>
+#define __gl_h_
+#define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
+#include <OpenGL/gl3.h>
 #else
-# include <GL/glew.h>
+#include <GL/glew.h>
 #endif
 
 #ifdef GL_TRANSITION_USING_EGL
-# include <EGL/egl.h>
-# include <EGL/eglext.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
 #else
-# include <GLFW/glfw3.h>
+#include <GLFW/glfw3.h>
 #endif
 
 #include <stdio.h>
@@ -32,7 +32,7 @@
 #include <float.h>
 
 #define FROM (0)
-#define TO   (1)
+#define TO (1)
 
 #define PIXEL_FORMAT (GL_RGB)
 
@@ -47,51 +47,51 @@ static const EGLint configAttribs[] = {
     EGL_NONE};
 #endif
 static const float position[12] = {
-  -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f
-};
+    -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f};
 
 static const GLchar *v_shader_source =
-  "attribute vec2 position;\n"
-  "varying vec2 _uv;\n"
-  "void main(void) {\n"
-  "  gl_Position = vec4(position, 0, 1);\n"
-  "  vec2 uv = position * 0.5 + 0.5;\n"
-  "  _uv = vec2(uv.x, 1.0 - uv.y);\n"
-  "}\n";
+    "attribute vec2 position;\n"
+    "varying vec2 _uv;\n"
+    "void main(void) {\n"
+    "  gl_Position = vec4(position, 0, 1);\n"
+    "  vec2 uv = position * 0.5 + 0.5;\n"
+    "  _uv = vec2(uv.x, 1.0 - uv.y);\n"
+    "}\n";
 
 static const GLchar *f_shader_template =
-  "varying vec2 _uv;\n"
-  "uniform sampler2D from;\n"
-  "uniform sampler2D to;\n"
-  "uniform float progress;\n"
-  "uniform float ratio;\n"
-  "uniform float _fromR;\n"
-  "uniform float _toR;\n"
-  "\n"
-  "vec4 getFromColor(vec2 uv) {\n"
-  "  return texture2D(from, vec2(uv.x, 1.0 - uv.y));\n"
-  "}\n"
-  "\n"
-  "vec4 getToColor(vec2 uv) {\n"
-  "  return texture2D(to, vec2(uv.x, 1.0 - uv.y));\n"
-  "}\n"
-  "\n"
-  "\n%s\n"
-  "void main() {\n"
-  "  gl_FragColor = transition(_uv);\n"
-  "}\n";
+    "varying vec2 _uv;\n"
+    "uniform sampler2D from;\n"
+    "uniform sampler2D to;\n"
+    "uniform float progress;\n"
+    "uniform float ratio;\n"
+    "uniform float _fromR;\n"
+    "uniform float _toR;\n"
+    "\n"
+    "vec4 getFromColor(vec2 uv) {\n"
+    "  return texture2D(from, vec2(uv.x, 1.0 - uv.y));\n"
+    "}\n"
+    "\n"
+    "vec4 getToColor(vec2 uv) {\n"
+    "  return texture2D(to, vec2(uv.x, 1.0 - uv.y));\n"
+    "}\n"
+    "\n"
+    "\n%s\n"
+    "void main() {\n"
+    "  gl_FragColor = transition(_uv);\n"
+    "}\n";
 
 // default to a basic fade effect
 static const GLchar *f_default_transition_source =
-  "vec4 transition (vec2 uv) {\n"
-  "  return mix(\n"
-  "    getFromColor(uv),\n"
-  "    getToColor(uv),\n"
-  "    progress\n"
-  "  );\n"
-  "}\n";
+    "vec4 transition (vec2 uv) {\n"
+    "  return mix(\n"
+    "    getFromColor(uv),\n"
+    "    getToColor(uv),\n"
+    "    progress\n"
+    "  );\n"
+    "}\n";
 
-typedef struct {
+typedef struct
+{
   const AVClass *class;
   FFFrameSync fs;
 
@@ -104,44 +104,44 @@ typedef struct {
   int64_t first_pts;
 
   // uniforms
-  GLuint        from;
-  GLuint        to;
-  GLint         progress;
-  GLint         ratio;
-  GLint         _fromR;
-  GLint         _toR;
+  GLuint from;
+  GLuint to;
+  GLint progress;
+  GLint ratio;
+  GLint _fromR;
+  GLint _toR;
 
   // internal state
-  GLuint        posBuf;
-  GLuint        program;
+  GLuint posBuf;
+  GLuint program;
 #ifdef GL_TRANSITION_USING_EGL
   EGLDisplay eglDpy;
   EGLConfig eglCfg;
   EGLSurface eglSurf;
   EGLContext eglCtx;
 #else
-  GLFWwindow    *window;
+  GLFWwindow *window;
 #endif
 
   GLchar *f_shader_source;
 } GLTransitionContext;
 
 #define OFFSET(x) offsetof(GLTransitionContext, x)
-#define FLAGS AV_OPT_FLAG_FILTERING_PARAM|AV_OPT_FLAG_VIDEO_PARAM
+#define FLAGS AV_OPT_FLAG_FILTERING_PARAM | AV_OPT_FLAG_VIDEO_PARAM
 
 static const AVOption gltransition_options[] = {
-  { "duration", "transition duration in seconds", OFFSET(duration), AV_OPT_TYPE_DOUBLE, {.dbl=1.0}, 0, DBL_MAX, FLAGS },
-  { "offset", "delay before startingtransition in seconds", OFFSET(offset), AV_OPT_TYPE_DOUBLE, {.dbl=0.0}, 0, DBL_MAX, FLAGS },
-  { "source", "path to the gl-transition source file (defaults to basic fade)", OFFSET(source), AV_OPT_TYPE_STRING, {.str = NULL}, CHAR_MIN, CHAR_MAX, FLAGS },
-  {NULL}
-};
+    {"duration", "transition duration in seconds", OFFSET(duration), AV_OPT_TYPE_DOUBLE, {.dbl = 1.0}, 0, DBL_MAX, FLAGS},
+    {"offset", "delay before startingtransition in seconds", OFFSET(offset), AV_OPT_TYPE_DOUBLE, {.dbl = 0.0}, 0, DBL_MAX, FLAGS},
+    {"source", "path to the gl-transition source file (defaults to basic fade)", OFFSET(source), AV_OPT_TYPE_STRING, {.str = NULL}, CHAR_MIN, CHAR_MAX, FLAGS},
+    {NULL}};
 
 FRAMESYNC_DEFINE_CLASS(gltransition, GLTransitionContext, fs);
 
 static GLuint build_shader(AVFilterContext *ctx, const GLchar *shader_source, GLenum type)
 {
   GLuint shader = glCreateShader(type);
-  if (!shader || !glIsShader(shader)) {
+  if (!shader || !glIsShader(shader))
+  {
     return 0;
   }
 
@@ -159,17 +159,20 @@ static int build_program(AVFilterContext *ctx)
   GLuint v_shader, f_shader;
   GLTransitionContext *c = ctx->priv;
 
-  if (!(v_shader = build_shader(ctx, v_shader_source, GL_VERTEX_SHADER))) {
+  if (!(v_shader = build_shader(ctx, v_shader_source, GL_VERTEX_SHADER)))
+  {
     av_log(ctx, AV_LOG_ERROR, "invalid vertex shader\n");
     return -1;
   }
 
   char *source = NULL;
 
-  if (c->source) {
+  if (c->source)
+  {
     FILE *f = fopen(c->source, "rb");
 
-    if (!f) {
+    if (!f)
+    {
       av_log(ctx, AV_LOG_ERROR, "invalid transition source file \"%s\"\n", c->source);
       return -1;
     }
@@ -189,19 +192,22 @@ static int build_program(AVFilterContext *ctx)
 
   int len = strlen(f_shader_template) + strlen(transition_source);
   c->f_shader_source = av_calloc(len, sizeof(*c->f_shader_source));
-  if (!c->f_shader_source) {
+  if (!c->f_shader_source)
+  {
     return AVERROR(ENOMEM);
   }
 
   snprintf(c->f_shader_source, len * sizeof(*c->f_shader_source), f_shader_template, transition_source);
-  av_log(ctx, AV_LOG_DEBUG, "\n%s\n", c->f_shader_source);
+  av_log(ctx, AV_LOG_DEBUG, "shader_source\n%s\n", c->f_shader_source);
 
-  if (source) {
+  if (source)
+  {
     free(source);
     source = NULL;
   }
 
-  if (!(f_shader = build_shader(ctx, c->f_shader_source, GL_FRAGMENT_SHADER))) {
+  if (!(f_shader = build_shader(ctx, c->f_shader_source, GL_FRAGMENT_SHADER)))
+  {
     av_log(ctx, AV_LOG_ERROR, "invalid fragment shader\n");
     return -1;
   }
@@ -229,7 +235,7 @@ static void setup_vbo(GLTransitionContext *c)
 
 static void setup_tex(AVFilterLink *fromLink)
 {
-  AVFilterContext     *ctx = fromLink->dst;
+  AVFilterContext *ctx = fromLink->dst;
   GLTransitionContext *c = ctx->priv;
 
   { // from
@@ -265,7 +271,7 @@ static void setup_tex(AVFilterLink *fromLink)
 
 static void setup_uniforms(AVFilterLink *fromLink)
 {
-  AVFilterContext     *ctx = fromLink->dst;
+  AVFilterContext *ctx = fromLink->dst;
   GLTransitionContext *c = ctx->priv;
 
   c->progress = glGetUniformLocation(c->program, "progress");
@@ -288,23 +294,22 @@ static int setup_gl(AVFilterLink *inLink)
   AVFilterContext *ctx = inLink->dst;
   GLTransitionContext *c = ctx->priv;
 
-
 #ifdef GL_TRANSITION_USING_EGL
-  //init EGL
-  // 1. Initialize EGL
-  // c->eglDpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+  // init EGL
+  //  1. Initialize EGL
+  //  c->eglDpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
 
-  #define MAX_DEVICES 4
+#define MAX_DEVICES 4
   EGLDeviceEXT eglDevs[MAX_DEVICES];
   EGLint numDevices;
 
-  PFNEGLQUERYDEVICESEXTPROC eglQueryDevicesEXT =(PFNEGLQUERYDEVICESEXTPROC)
-  eglGetProcAddress("eglQueryDevicesEXT");
+  PFNEGLQUERYDEVICESEXTPROC eglQueryDevicesEXT = (PFNEGLQUERYDEVICESEXTPROC)
+      eglGetProcAddress("eglQueryDevicesEXT");
 
   eglQueryDevicesEXT(MAX_DEVICES, eglDevs, &numDevices);
 
-  PFNEGLGETPLATFORMDISPLAYEXTPROC eglGetPlatformDisplayEXT =  (PFNEGLGETPLATFORMDISPLAYEXTPROC)
-  eglGetProcAddress("eglGetPlatformDisplayEXT");
+  PFNEGLGETPLATFORMDISPLAYEXTPROC eglGetPlatformDisplayEXT = (PFNEGLGETPLATFORMDISPLAYEXTPROC)
+      eglGetProcAddress("eglGetPlatformDisplayEXT");
 
   c->eglDpy = eglGetPlatformDisplayEXT(EGL_PLATFORM_DEVICE_EXT, eglDevs[0], 0);
 
@@ -330,11 +335,12 @@ static int setup_gl(AVFilterLink *inLink)
   c->eglCtx = eglCreateContext(c->eglDpy, c->eglCfg, EGL_NO_CONTEXT, NULL);
   eglMakeCurrent(c->eglDpy, c->eglSurf, c->eglSurf, c->eglCtx);
 #else
-  //glfw
+  // glfw
 
   glfwWindowHint(GLFW_VISIBLE, 0);
   c->window = glfwCreateWindow(inLink->w, inLink->h, "", NULL, NULL);
-  if (!c->window) {
+  if (!c->window)
+  {
     av_log(ctx, AV_LOG_ERROR, "setup_gl ERROR");
     return -1;
   }
@@ -350,7 +356,8 @@ static int setup_gl(AVFilterLink *inLink)
   glViewport(0, 0, inLink->w, inLink->h);
 
   int ret;
-  if((ret = build_program(ctx)) < 0) {
+  if ((ret = build_program(ctx)) < 0)
+  {
     return ret;
   }
 
@@ -374,7 +381,8 @@ static AVFrame *apply_transition(FFFrameSync *fs,
   AVFrame *outFrame;
 
   outFrame = ff_get_video_buffer(outLink, outLink->w, outLink->h);
-  if (!outFrame) {
+  if (!outFrame)
+  {
     return NULL;
   }
 
@@ -390,7 +398,7 @@ static AVFrame *apply_transition(FFFrameSync *fs,
 
   const float ts = ((fs->pts - c->first_pts) / (float)fs->time_base.den) - c->offset;
   const float progress = FFMAX(0.0f, FFMIN(1.0f, ts / c->duration));
-  // av_log(ctx, AV_LOG_ERROR, "transition '%s' %llu %f %f\n", c->source, fs->pts - c->first_pts, ts, progress);
+  av_log(ctx, AV_LOG_ERROR, "transition '%s' %llu %f %f\n", c->source, fs->pts - c->first_pts, ts, progress);
   glUniform1f(c->progress, progress);
 
   glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
@@ -427,20 +435,24 @@ static int blend_frame(FFFrameSync *fs)
   int ret;
 
   ret = ff_framesync_dualinput_get(fs, &fromFrame, &toFrame);
-  if (ret < 0) {
+  if (ret < 0)
+  {
     return ret;
   }
 
-  if (c->first_pts == AV_NOPTS_VALUE && fromFrame && fromFrame->pts != AV_NOPTS_VALUE) {
+  if (c->first_pts == AV_NOPTS_VALUE && fromFrame && fromFrame->pts != AV_NOPTS_VALUE)
+  {
     c->first_pts = fromFrame->pts;
   }
 
-  if (!toFrame) {
+  if (!toFrame)
+  {
     return ff_filter_frame(ctx->outputs[0], fromFrame);
   }
 
   outFrame = apply_transition(fs, ctx, fromFrame, toFrame);
-  if (!outFrame) {
+  if (!outFrame)
+  {
     return AVERROR(ENOMEM);
   }
 
@@ -453,7 +465,6 @@ static av_cold int init(AVFilterContext *ctx)
   c->fs.on_event = blend_frame;
   c->first_pts = AV_NOPTS_VALUE;
 
-
 #ifndef GL_TRANSITION_USING_EGL
   if (!glfwInit())
   {
@@ -464,12 +475,14 @@ static av_cold int init(AVFilterContext *ctx)
   return 0;
 }
 
-static av_cold void uninit(AVFilterContext *ctx) {
+static av_cold void uninit(AVFilterContext *ctx)
+{
   GLTransitionContext *c = ctx->priv;
   ff_framesync_uninit(&c->fs);
 
 #ifdef GL_TRANSITION_USING_EGL
-  if (c->eglDpy) {
+  if (c->eglDpy)
+  {
     glDeleteTextures(1, &c->from);
     glDeleteTextures(1, &c->to);
     glDeleteBuffers(1, &c->posBuf);
@@ -477,7 +490,8 @@ static av_cold void uninit(AVFilterContext *ctx) {
     eglTerminate(c->eglDpy);
   }
 #else
-  if (c->window) {
+  if (c->window)
+  {
     glDeleteTextures(1, &c->from);
     glDeleteTextures(1, &c->to);
     glDeleteBuffers(1, &c->posBuf);
@@ -486,7 +500,8 @@ static av_cold void uninit(AVFilterContext *ctx) {
   }
 #endif
 
-  if (c->f_shader_source) {
+  if (c->f_shader_source)
+  {
     av_freep(&c->f_shader_source);
   }
 }
@@ -494,9 +509,8 @@ static av_cold void uninit(AVFilterContext *ctx) {
 static int query_formats(AVFilterContext *ctx)
 {
   static const enum AVPixelFormat formats[] = {
-    AV_PIX_FMT_RGB24,
-    AV_PIX_FMT_NONE
-  };
+      AV_PIX_FMT_RGB24,
+      AV_PIX_FMT_NONE};
 
   return ff_set_common_formats(ctx, ff_make_format_list(formats));
 }
@@ -515,15 +529,17 @@ static int config_output(AVFilterLink *outLink)
   AVFilterLink *toLink = ctx->inputs[TO];
   int ret;
 
-  if (fromLink->format != toLink->format) {
+  if (fromLink->format != toLink->format)
+  {
     av_log(ctx, AV_LOG_ERROR, "inputs must be of same pixel format\n");
     return AVERROR(EINVAL);
   }
 
-  if (fromLink->w != toLink->w || fromLink->h != toLink->h) {
+  if (fromLink->w != toLink->w || fromLink->h != toLink->h)
+  {
     av_log(ctx, AV_LOG_ERROR, "First input link %s parameters "
-           "(size %dx%d) do not match the corresponding "
-           "second input link %s parameters (size %dx%d)\n",
+                              "(size %dx%d) do not match the corresponding "
+                              "second input link %s parameters (size %dx%d)\n",
            ctx->input_pads[FROM].name, fromLink->w, fromLink->h,
            ctx->input_pads[TO].name, toLink->w, toLink->h);
     return AVERROR(EINVAL);
@@ -534,7 +550,8 @@ static int config_output(AVFilterLink *outLink)
   // outLink->time_base = fromLink->time_base;
   outLink->frame_rate = fromLink->frame_rate;
 
-  if ((ret = ff_framesync_init_dualinput(&c->fs, ctx)) < 0) {
+  if ((ret = ff_framesync_init_dualinput(&c->fs, ctx)) < 0)
+  {
     return ret;
   }
 
@@ -542,38 +559,33 @@ static int config_output(AVFilterLink *outLink)
 }
 
 static const AVFilterPad gltransition_inputs[] = {
-  {
-    .name = "from",
-    .type = AVMEDIA_TYPE_VIDEO,
-    .config_props = setup_gl,
-  },
-  {
-    .name = "to",
-    .type = AVMEDIA_TYPE_VIDEO,
-  },
-  {NULL}
-};
+    {
+        .name = "from",
+        .type = AVMEDIA_TYPE_VIDEO,
+        .config_props = setup_gl,
+    },
+    {
+        .name = "to",
+        .type = AVMEDIA_TYPE_VIDEO,
+    }};
 
 static const AVFilterPad gltransition_outputs[] = {
-  {
-    .name = "default",
-    .type = AVMEDIA_TYPE_VIDEO,
-    .config_props = config_output,
-  },
-  {NULL}
-};
+    {
+        .name = "default",
+        .type = AVMEDIA_TYPE_VIDEO,
+        .config_props = config_output,
+    }};
 
 AVFilter ff_vf_gltransition = {
-  .name          = "gltransition",
-  .description   = NULL_IF_CONFIG_SMALL("OpenGL blend transitions"),
-  .priv_size     = sizeof(GLTransitionContext),
-  .preinit       = gltransition_framesync_preinit,
-  .init          = init,
-  .uninit        = uninit,
-  .query_formats = query_formats,
-  .activate      = activate,
-  .inputs        = gltransition_inputs,
-  .outputs       = gltransition_outputs,
-  .priv_class    = &gltransition_class,
-  .flags         = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC
-};
+    .name = "gltransition",
+    .description = NULL_IF_CONFIG_SMALL("OpenGL blend transitions"),
+    .priv_size = sizeof(GLTransitionContext),
+    .preinit = gltransition_framesync_preinit,
+    .init = init,
+    .uninit = uninit,
+    FILTER_QUERY_FUNC(query_formats),
+    FILTER_INPUTS(gltransition_inputs),
+    FILTER_OUTPUTS(gltransition_outputs),
+    .activate = activate,
+    .priv_class = &gltransition_class,
+    .flags = AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC};
