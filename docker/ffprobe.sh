e1db0fb8a69c6a7d9a92ce8a8925fa73da002ec5
Michael Smirnov <michael@guidde.co>
2023-06-14 16:29:20 -0600

    add docker

---
+++ docker/ffprobe.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run the command passed as arguments with the current directory mounted
+# to the same directory inside the container
+docker run -v "$(pwd)":/app -v /tmp:/tmp nadav-mac ffprobe "$@"
